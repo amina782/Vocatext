79df03ef5d213fb02c6e83079fc2cecbae2fd1af
amina782 <aminafirdose4@gmail.com>
2025-03-09 01:10:10 +0530

    added contact page

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
    app.run(debug=True) 
