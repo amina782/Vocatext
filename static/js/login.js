fda6382c8102d421c31ad371447a83dd64843e7a
amina782 <aminafirdose4@gmail.com>
2025-03-13 00:04:48 +0530

    updated

--- static/js/login.js
+++ static/js/login.js
@@ -1,9 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
 const container=document.querySelector('.container');
 const LoginLink=document.querySelector('.SignInLink')
 const RegisterLink=document.querySelector('.SignUpLink')
 RegisterLink.addEventListener('click',()=>{
     container.classList.add('active');
-})
+});
 LoginLink.addEventListener('click',()=>{
     container.classList.remove('active');
-})
+});
+});
